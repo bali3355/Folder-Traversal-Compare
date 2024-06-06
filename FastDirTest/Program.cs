16a7cfed608f8d2db5517040f189c39caeb7d0cc
Bazs <mmx0gj@stud.uni-obuda.hu>
2024-06-06 21:05:39 +0200

    Add project files.

---
+++ FastDirTest/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FastDirTest;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+var searchPath = @"C:\Users\";
+var sw = Stopwatch.StartNew();
+var list = FastFileInfo.EnumerateDirectories(searchPath, "*", SearchOption.AllDirectories);
+sw.Stop();
+var firsttime = sw.Elapsed;
+Parallel.ForEach(list, (item) => { Console.WriteLine(item.FullName); });
+Console.WriteLine($"Result time({searchPath}):\n{firsttime} | {list.Count()}");
+Console.ReadKey();
