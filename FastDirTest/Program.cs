300b6721233c6df4837279462cf592c208b8ea75
Bazs <mmx0gj@stud.uni-obuda.hu>
2024-06-10 12:44:36 +0200

    #1.1
    Updated methods and added new features for file enumeration
    
    Updated `End` method in `StopwatchExtensions` to return only elapsed `TimeSpan`. Enhanced `Main` method to include formatted console output and user exit prompt. Overloaded `TestEnumeratingFiles` to handle different `IEnumerable` inputs and updated it along with `Main` method to use the new `End` method. Added `V1GetFiles` and `V2GetFiles` methods for parallel file searching. Improved console output for better detail in `Main` and `TestEnumeratingFiles` methods.

--- FastDirTest/Program.cs
+++ FastDirTest/Program.cs
@@ -8,22 +8,135 @@ namespace FastDirTest
 {
     public static class StopwatchExtensions
     {
-        public static (Stopwatch, TimeSpan) End(this Stopwatch stopwatch)
+        public static TimeSpan End(this Stopwatch stopwatch)
         {
-            if (!stopwatch.IsRunning) return (stopwatch, TimeSpan.Zero);
+            if (!stopwatch.IsRunning) return TimeSpan.Zero;
             var time = stopwatch.Elapsed;
             stopwatch.Reset();
-            return (stopwatch, time);
+            return time;
 
         }
     }
     internal class Program
     {
+        internal const int nameWidth = 30, timeWidth = 30, countWidth = 30;
         internal static Stopwatch stopwatch = new();
         internal static int Count { get; set; }
         internal static string SearchPath { get; } = @"C:\";
         internal static string[] Separator { get; } = ["\r\n"];
         internal static TimeSpan Time { get; set; }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!\n");
+            string header = $"| {"Enumerator name",-nameWidth} | {"Enumerating Time",-timeWidth} | {"Enumerated Count",-countWidth} |";
+            Console.WriteLine(header);
+            Console.WriteLine(new string('-', header.Length));
+            
+            stopwatch.Start();
+            TestEnumeratingFiles(FastFileInfo.EnumerateFiles(SearchPath, "*", SearchOption.AllDirectories, null), "FastFileInfo");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(FastFileInfo.GetFiles(SearchPath, "*", SearchOption.AllDirectories), "FastFileInfoGetFiles");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(FastFileInfoV2.EnumerateFiles(SearchPath, "*", SearchOption.AllDirectories, null), "FastFileInfoV2");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(FastFileInfoV3.GetAllFiles(SearchPath, -1, SearchPath.Split('\\').Length), "FastFileInfoV3.GetAllFiles");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(FastFileInfoV3.GetAllFilesV2(SearchPath), "FastFileInfoV3.GetAllFilesV2");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(FastFileInfoV3.GetAllFilesV3(SearchPath), "FastFileInfoV3.GetAllFilesV3");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(V1GetFiles(SearchPath), "V1GetFiles");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(V2GetFiles(SearchPath), "V2GetFiles");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(GetAllFilesWithCMD(SearchPath), "GetAllFilesWithCMD");
+
+            stopwatch.Start();
+            TestEnumeratingFiles(GetAllFilesWithPowerShell(SearchPath), "GetAllFilesWithPowerShell");
+            Console.WriteLine(new string('-', header.Length));
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void TestEnumeratingFiles(IEnumerable<string> listOfFiles, string name)
+        {
+            Count = listOfFiles.Count();
+            Time = stopwatch.End();
+            Console.WriteLine($"| {name,-nameWidth} | {Time,-timeWidth} | {Count,-countWidth} |");
+        }
+        private static void TestEnumeratingFiles(IEnumerable<FastFileInfo> listOfFiles, string name)
+        {
+            Count = listOfFiles.Count();
+            Time = stopwatch.End();
+            Console.WriteLine($"| {name,-nameWidth} | {Time,-timeWidth} | {Count,-countWidth} |");
+
+        }
+        private static void TestEnumeratingFiles(IEnumerable<FastFileInfoV2> listOfFiles, string name)
+        {
+            Count = listOfFiles.Count();
+            Time = stopwatch.End();
+            Console.WriteLine($"| {name,-nameWidth} | {Time,-timeWidth} | {Count,-countWidth} |");
+
+        }
+        private static void TestEnumeratingFiles(IEnumerable<FastFileInfoV3> listOfFiles, string name)
+        {
+            Count = listOfFiles.Count();
+            Time = stopwatch.End();
+            Console.WriteLine($"| {name,-nameWidth} | {Time,-timeWidth} | {Count,-countWidth} |");
+        }
+        private static IEnumerable<string> V1GetFiles(string path)
+        {
+            //https://stackoverflow.com/questions/2106877/is-there-a-faster-way-than-this-to-find-all-the-files-in-a-directory-and-all-sub
+            ConcurrentQueue<string> pendingQueue = [];
+            ConcurrentBag<string> filesNames = [];
+            pendingQueue.Enqueue(path);
+
+            while (!pendingQueue.IsEmpty)
+            {
+                try
+                {
+                    pendingQueue.TryDequeue(out path);
+                    Parallel.ForEach(Directory.GetFiles(path), filesNames.Add);
+                    var dirs = Directory.GetDirectories(path);
+                    //If you need folders too
+                    //Parallel.ForEach(dirs, filesNames.Add);
+                    Parallel.ForEach(dirs, pendingQueue.Enqueue);
+                }
+                catch (Exception) { continue; }
+            }
+            return filesNames;
+        }
+        private static IEnumerable<string> V2GetFiles(string path)
+        {
+            var filesBag = new ConcurrentBag<string>();
+
+            try
+            {
+                var dirs = Directory.EnumerateDirectories(path, "*", SearchOption.TopDirectoryOnly).AsParallel();
+                var files = Directory.EnumerateFiles(path, "*", SearchOption.TopDirectoryOnly).AsParallel();
+                Parallel.ForEach(files, filesBag.Add);
+                //If you need folders too
+                //Parallel.ForEach(dirs, filesBag.Add);
+                Parallel.ForEach(dirs, dir =>
+                {
+                    try { Parallel.ForEach(V2GetFiles(dir), filesBag.Add); }
+                    catch { return; }
+                });
+            }
+            catch
+            { return []; }
+
+            return filesBag;
+        }
         private static IEnumerable<string> GetAllDirectoriesWithCMD(string searchPath)
         {
             using var process = new Process
@@ -44,7 +157,6 @@ namespace FastDirTest
 
             return [.. cmdOutput.Split(Separator, StringSplitOptions.RemoveEmptyEntries)];
         }
-
         private static IEnumerable<string> GetAllFilesWithCMD(string searchPath)
         {
             using var process = new Process
@@ -65,7 +177,6 @@ namespace FastDirTest
 
             return [.. cmdOutput.Split(Separator, StringSplitOptions.RemoveEmptyEntries)];
         }
-
         private static IEnumerable<string> GetAllFilesWithPowerShell(string searchPath)
         {
             var powershellArgs = @"
@@ -102,7 +213,6 @@ namespace FastDirTest
 
             return [.. cmdOutput.Split("\n", StringSplitOptions.RemoveEmptyEntries)];
         }
-
         private static IEnumerable<string> GetAllFoldersWithPowerShell(string searchPath)
         {
             var powershellArgs = @"
@@ -135,108 +245,6 @@ namespace FastDirTest
             process.WaitForExit();
 
             return [.. cmdOutput.Split("\n", StringSplitOptions.RemoveEmptyEntries)];
-        }
-
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(FastFileInfo.EnumerateFiles(SearchPath, "*", SearchOption.AllDirectories, null), "FastFileInfo");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(FastFileInfo.GetFiles(SearchPath, "*", SearchOption.AllDirectories), "FastFileInfoGetFiles");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(FastFileInfoV2.EnumerateFiles(SearchPath, "*", SearchOption.AllDirectories, null), "FastFileInfoV2");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(FastFileInfoV3.GetAllFiles(SearchPath, -1, SearchPath.Split('\\').Length), "FastFileInfoV3.GetAllFiles");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(FastFileInfoV3.GetAllFilesV2(SearchPath), "FastFileInfoV3.GetAllFilesV2");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(FastFileInfoV3.GetAllFilesV3(SearchPath), "FastFileInfoV3.GetAllFilesV3");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(V1GetFiles(SearchPath), "V1GetFiles");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(V2GetFiles(SearchPath), "V2GetFiles");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(GetAllFilesWithCMD(SearchPath), "GetAllFilesWithCMD");
-
-            stopwatch.Start();
-            TestEnumeratingFiles(GetAllFilesWithPowerShell(SearchPath), "GetAllFilesWithPowerShell");
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
-
-        private static void TestEnumeratingFiles(IEnumerable<string> listOfFiles, string name)
-        {
-            Count = listOfFiles.Count();
-            Time = stopwatch.End().Item2;
-            Console.WriteLine($"Result Time({name}): {Time} count: {Count}");
-        }
-        private static void TestEnumeratingFiles(IEnumerable<FastFileInfo> listOfFiles, string name)
-        {
-            Count = listOfFiles.Count();
-            Time = stopwatch.End().Item2;
-            Console.WriteLine($"Result Time({name}): {Time} count: {Count}");
-        }
-        private static void TestEnumeratingFiles(IEnumerable<FastFileInfoV2> listOfFiles, string name)
-        {
-            Count = listOfFiles.Count();
-            Time = stopwatch.End().Item2;
-            Console.WriteLine($"Result Time({name}): {Time} count: {Count}");
-        }
-        private static void TestEnumeratingFiles(IEnumerable<FastFileInfoV3> listOfFiles, string name)
-        {
-            Count = listOfFiles.Count();
-            Time = stopwatch.End().Item2;
-            Console.WriteLine($"Result Time({name}): {Time} count: {Count}");
-        }
-        private static IEnumerable<string> V1GetFiles(string path)
-        {
-            //https://stackoverflow.com/questions/2106877/is-there-a-faster-way-than-this-to-find-all-the-files-in-a-directory-and-all-sub
-            ConcurrentQueue<string> pendingQueue = [];
-            ConcurrentBag<string> filesNames = [];
-            pendingQueue.Enqueue(path);
-
-            while (!pendingQueue.IsEmpty)
-            {
-                try
-                {
-                    pendingQueue.TryDequeue(out path);
-                    Parallel.ForEach(Directory.GetFiles(path), filesNames.Add);
-                    Parallel.ForEach(Directory.GetDirectories(path), pendingQueue.Enqueue);
-                }
-                catch (Exception) { continue; }
-            }
-            return filesNames;
-        }
-        private static IEnumerable<string> V2GetFiles(string path)
-        {
-            var filesBag = new ConcurrentBag<string>();
-
-            try
-            {
-                var dirs = Directory.EnumerateDirectories(path, "*", SearchOption.TopDirectoryOnly).AsParallel();
-                var files = Directory.EnumerateFiles(path, "*", SearchOption.TopDirectoryOnly).AsParallel();
-                Parallel.ForEach(files, filesBag.Add);
-                Parallel.ForEach(dirs, dir =>
-                {
-                    try { Parallel.ForEach(V2GetFiles(dir), filesBag.Add); }
-                    catch { return; }
-                });
-            }
-            catch
-            { return []; }
-
-            return filesBag;
         }
 
     }
